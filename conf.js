1819fd27404d04cea66c791b0d7796a576d053df
jyg <hahayile9@gmail.com>
2020-04-09 15:06:27 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -12,7 +12,7 @@ jsproxy_config({
     'defalut': {
       label: '第1通道',
       lines: {
-        '111.111.workers.dev': 1,
+        'https://tight-mountain-96fe.juyuange.workers.dev': 1,
       }
     },
     
@@ -20,7 +20,7 @@ jsproxy_config({
     'defalut1': {
       label: '第2通道',
       lines: {
-        '222.222.workers.dev': 1,
+        'https://tight-mountain-96fe.juyuange.workers.dev': 1,
       }
     },
     
